e526abb7accbc383a5ab628615fe15e498d55437
andamira16 <100887721+andamira16@users.noreply.github.com>
2022-04-22 09:05:37 +0700

    Delete MariaDB.sql

--- UTS Praktikum/MariaDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `penyerahan` (
-  `id_penyerahan` int(11) NOT NULL,
-  `id_pembelian` INT(11) NOT NULL,
-  `tanggal` int(8) NOT NULL,
-  `metode` Varchar(7)NOT NULL,
-  `statuspembayaran`varchar(7) NOT NULL,
-  PRIMARY KEY (`id_penyerahan`)
-)
